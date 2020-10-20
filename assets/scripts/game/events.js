cde207bf825e0609653845738704a78a3f351a55
Crookinator <a_crook@icloud.com>
2020-10-20 11:23:50 -0400

    cleaned up events
    cleaned up the code in the events file to make it more readable

--- assets/scripts/game/events.js
+++ assets/scripts/game/events.js
@@ -4,60 +4,35 @@ const gameUi = require('./ui')
 const store = require('../store')
 
 
-
-// set the winning combinations to an array
-const winningCombos = [
-	[0, 1, 2],
-	[3, 4, 5],
-	[6, 7, 8],
-	[0, 3, 6],
-	[1, 4, 7],
-	[2, 5, 8],
-	[0, 4, 8],
-	[2, 4, 6]
-]
-
-// set over to false
-let over = false
+// set isOver to false
+let isOver = false
 	
-	// set current player
+// set current player
 let currentPlayer = 'X'
 
 // box click event handler
 const onBoxClick = (event) => {
 
-	// use event.target to select the specific box clicked
-	const box = $(event.target)
+// use event.target to select the specific box clicked
+const box = $(event.target)
 
-	//disable future clicks on box and set a class of clicked 
-	$(box).prop('disabled', true)
-	$(box).addClass('clicked')
+//disable future clicks on box and set a class of clicked 
+$(box).prop('disabled', true)
+$(box).addClass('clicked')
 
-	// set text inside box to current players value
-	box.text(currentPlayer)
-	
-	//set store.game.cells to above move 
-	store.game.cells[box.data('cell-index')] = currentPlayer
-	
-	// logic for game over or tie
-		let tie = store.game.cells.includes('')
-	if (store.game.cells.slice(0, 3).every(e => e === currentPlayer)) {
-		console.log('its working')
-		over = true
-		}
-		//pass box[index], currentPlayer, and over to gameUpdate
-	gameApi.gameUpdate(box.data('cell-index'), currentPlayer, false)
-		.then(gameUi.onUpdateSuccess)
-		.catch(gameUi.onError)
-	
-	if (!tie === true) {
-				$('#userAlert').text("It's a tie!")
-			}else if (over === true) {
-				$('#userAlert').text(currentPlayer + ' WINS!... Want to play again? click Start New Game')
-			}else {
-				//ternary opporator to switch currentPlayer between X and O
-				currentPlayer = currentPlayer === 'O' ? 'X' : 'O'
-			}
+// set text inside box to current players value
+box.text(currentPlayer)
+
+//set store.game.cells to above move 
+store.game.cells[box.data('cell-index')] = currentPlayer
+
+//pass box[index], currentPlayer, and over to gameUpdate
+gameApi.gameUpdate(box.data('cell-index'), currentPlayer, false)
+.then(gameUi.onUpdateSuccess)
+.catch(gameUi.onError)
+
+//ternary opporator to switch currentPlayer between X and O
+currentPlayer = currentPlayer === 'O' ? 'X' : 'O'
 }
 
 // when play is clicked
